1887c2b735890912ed497ad6a98b6ecc2e058933
CookieHunt <160796036+CookieHunt@users.noreply.github.com>
2024-02-22 19:46:17 +0100

    Add window
    Simple window deploy

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print('Hello World!')
+from tkinter import *
+import math
+
+
+def main():
+    print('Hello World!')
+
+    window = Tk()
+    window.title('Calculator')
+    window.geometry('400x600')
+
+    icon = PhotoImage(file='icon.png')
+    window.iconphoto(True, icon)
+
+    window.mainloop()
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
